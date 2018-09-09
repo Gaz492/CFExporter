74c76980a20e131fe3d16aa08024de02f2546a19
Gareth Williams <gaz492@gmail.com>
2018-09-09 01:47:52 +0100

    manifest.json struct

--- structs.go
+++ structs.go
@@ -7,3 +7,30 @@ type buildJson struct {
 	ModsFolder string `json:"modsFolder"`
 	Includes []string `json:"includes"`
 }
+
+type manifestBase struct {
+	Minecraft manifestMinecraft `json:"minecraft"`
+	ManifestType string `json:"manifestType"`
+	ManifestVersion string `json:"manifestVersion"`
+	Name string `json:"name"`
+	Version string `json:"version"`
+	Author string `json:"author"`
+	Files []maifestFiles `json:"files"`
+	overrides string `json:"overrides"`
+}
+
+type manifestMinecraft struct {
+	Version string `json:"version"`
+	ModLoaders []manifestMinecraftModLoaders `json:"modLoaders"`
+}
+
+type manifestMinecraftModLoaders struct {
+	Id string `json:"id"`
+	Primary bool `json:"primary"`
+}
+
+type maifestFiles struct {
+	ProjectID int `json:"projectID"`
+	FileID int `json:"fileID"`
+	Required bool `json:"required"`
+} 
