e54da53b0388b2c1dc0a1c7b5f9c9106bfa01acd
Gareth Williams <gaz492@gmail.com>
2018-09-09 16:44:39 +0100

    prep for manifest.json creation

--- exporter.go
+++ exporter.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -10,18 +11,25 @@ import (
 	"strings"
 )
 
+var (
+	PROXY_API      = "https://curse.gaz492.uk/api/"
+	ProxyAuthToken *string
+	PackVersion    *string
+	ExportName     *string
+	BuildConfig    buildJson
+)
+
 func main() {
 	mcDirCLI := flag.String("d", "./", "Path to root folder of Minecraft instance")
-	//pVerCLI := flag.String("p", "1.0.0", "Pack Version (e.g 1.0.0)")
-	//exportName := flag.String("n", "Twitch-Export", "Export Name")
+	PackVersion = flag.String("p", "1.0.0", "Pack Version (e.g 1.0.0)")
+	ExportName = flag.String("n", "Twitch-Export", "Export Name")
 	buildConfig := flag.String("c", ".build.json", "Config file to get build variables")
+	ProxyAuthToken = flag.String("pt", "changeme", "Authentication token used to authenticate with Gaz's Twitch Proxy")
 	flag.Parse()
-	//fmt.Println("Pack Ver:", *pVerCLI)
-	//fmt.Println("Export Name:", *exportName)
+	fmt.Println("Pack Ver:", *PackVersion)
+	fmt.Println("Export Name:", *ExportName)
 
-	//fmt.Println(filepath.Abs(filepath.Dir(os.Args[0])))
-
-	readBuildJson(*buildConfig)
+	BuildConfig = readBuildJson(*buildConfig)
 	readMCDIR(*mcDirCLI)
 }
 
@@ -52,7 +60,28 @@ func listMods(modsFolder string) {
 			//fmt.Println(GetFileHash(path.Join(modsFolder, "mods", f.Name())))
 			fileHash, _ := GetFileHash(path.Join(modsFolder, "mods", f.Name()))
 			jarFingerprints = append(jarFingerprints, fileHash)
-			fmt.Printf("%v %v\n", jarFingerprints, f.Name())
+			//fmt.Printf("%v %v\n", jarFingerprints, f.Name())
 		}
 	}
+
+	//fMatchResp, _ := getProjectIds(jarFingerprints)
+
+	//var test2 []fingerprintExactMatches
+	//test2 = fMatchResp.ExactMatches
+	//fmt.Println(test2[0].Id)
+
+
+}
+
+func createExport(projectFiles []manifestFiles) {
+	var modloader []manifestMinecraftModLoaders
+	var tempFiles []manifestFiles
+	tempFiles = append(tempFiles, manifestFiles{1278361, 4522342, true})
+	modloader = append(modloader, manifestMinecraftModLoaders{"forge-" + BuildConfig.ForgeVersion, true})
+	manifestMc := manifestMinecraft{BuildConfig.MinecraftVersion, modloader}
+	manifestB := manifestBase{manifestMc, "minecraftModpack", 1, *ExportName, *PackVersion, BuildConfig.PackAuthor, tempFiles, "overrides"}
+	// test below
+	addonsJson, _ := json.Marshal(manifestB)
+	ioutil.WriteFile("output.json", addonsJson, 0644)
+	//fmt.Printf("%+v", jsonResp)
 }
