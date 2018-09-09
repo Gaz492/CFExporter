487fb491d15976fd3d8726233e7b55e3523b692d
Gareth Williams <gaz492@gmail.com>
2018-09-09 15:10:09 +0100

    fingerprint response struct

--- structs.go
+++ structs.go
@@ -8,6 +8,9 @@ type buildJson struct {
 	Includes []string `json:"includes"`
 }
 
+/*
+=================== Twitch Export Manifest ===================
+*/
 type manifestBase struct {
 	Minecraft manifestMinecraft `json:"minecraft"`
 	ManifestType string `json:"manifestType"`
@@ -33,4 +36,45 @@ type maifestFiles struct {
 	ProjectID int `json:"projectID"`
 	FileID int `json:"fileID"`
 	Required bool `json:"required"`
+}
+
+/*
+=================== Twitch Fingerprint Response ===================
+*/
+
+type fingerprintResponse struct {
+	IsCacheBuilt bool `json:"isCacheBuilt"`
+	ExactMatches []fingerprintExactMatches `json:"exactMatches"`
+	ExactFingerprints []int `json:"exactFingerprints"`
+	//PartialMatches []partialMatches `json:"partialMatches"`
+	//PartialMatchFingerprints partialMatchFingerprints `json:"partialMatchFingerprints"`
+	InstalledFingerprints []int `json:"installedFingerprints"`
+}
+
+type fingerprintExactMatches struct {
+	Id int `json:"id"`
+	File fingerprintFile `json:"file"`
+	LatestFiles []fingerprintFile `json:"latestFiles"`
+}
+
+type fingerprintFile struct {
+	Id int `json:"id"`
+	FileName string `json:"fileName"`
+	FileNameOnDisk string `json:"fileNameOnDisk"`
+	FileDate string `json:"fileDate"`
+	ReleaseType int `json:"releaseType"`
+	FileStatus int `json:"fileStatus"`
+	DownloadUrl string `json:"downloadUrl"`
+	IsAlternate bool `json:"isAlternate"`
+	AlternateFileId int `json:"alternateFileId"`
+	//Dependencies []fileDependencies `json:"dependencies"`
+	IsAvailable bool `json:"isAvailable"`
+	Modules []fileModules `json:"modules"`
+	PackageFingerprint int `json:"packageFingerprint"`
+	GameVersion []int `json:"gameVersion"`
+}
+
+type fileModules struct {
+	FolderName string `json:"folderName"`
+	Fingerprint int `json:"fimgerprint"`
 } 
