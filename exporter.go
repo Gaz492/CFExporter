269fa38acb2078ac6cc629bb49070b4af2c0b500
Gareth Williams <gaz492@gmail.com>
2018-11-19 21:51:32 +0000

    removed debug

--- exporter.go
+++ exporter.go
@@ -40,11 +40,9 @@ func main() {
 	if *ProxyAuthToken != "" && *CurseAuthToken == "" {
 		ApiUrl = "https://curse.gaz492.uk/api/"
 		fmt.Println("Using: " + ApiUrl + " as API")
-		fmt.Println(*ProxyAuthToken)
 	} else if *CurseAuthToken != "" && *ProxyAuthToken == "" {
-		ApiUrl = "https://addons-ecs.forgesvc.net"
+		ApiUrl = "https://addons-ecs.forgesvc.net/"
 		fmt.Println("Using: " + ApiUrl + " as API")
-		fmt.Println(*CurseAuthToken)
 	}
 
 	BuildConfig = readBuildJson(*buildConfig)
